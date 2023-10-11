825f70a38e37a736840cb352a4f509f02bb6d868
NohJooYoung <nohjoo97@naver.com>
2023-10-11 09:21:47 +0900

    10/10에 실습한 SimpleFirstPractice 파일 생성

---
+++ SimpleFirstPractice/SimpleFirstPractice/SimpleFirstPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleFirstPracticeApp.swift
+//  SimpleFirstPractice
+//
+//  Created by 노주영 on 2023/10/11.
+//
+
+import SwiftUI
+
+@main
+struct SimpleFirstPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
