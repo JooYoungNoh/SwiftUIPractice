0e382144357066eb9b70a7cf58e1e96e1e8ccf53
NohJooYoung <nohjoo97@naver.com>
2023-10-11 14:20:42 +0900

    예제 7) first commit

---
+++ SimpleSecoundPractice/SimpleSecoundPractice/ContentView2.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView2.swift
+//  SimpleSecoundPractice
+//
+//  Created by 노주영 on 2023/10/11.
+//
+
+import SwiftUI
+
+struct ContentView2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContentView2()
+}
