7039a50dfcc6375902913b806da7de7986c757b8
NohJooYoung <nohjoo97@naver.com>
2023-10-11 11:14:29 +0900

    10/11 실습 first commit

---
+++ SimpleSecoundPractice/SimpleSecoundPractice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SimpleSecoundPractice
+//
+//  Created by 노주영 on 2023/10/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SimpleSecoundPractice/SimpleSecoundPractice/SimpleSecoundPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleSecoundPracticeApp.swift
+//  SimpleSecoundPractice
+//
+//  Created by 노주영 on 2023/10/11.
+//
+
+import SwiftUI
+
+@main
+struct SimpleSecoundPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
