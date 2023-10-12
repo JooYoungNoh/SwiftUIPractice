8616b6424bd4adba4fdb5744fda2a98b361a1314
NohJooYoung <nohjoo97@naver.com>
2023-10-12 17:22:21 +0900

    예제 10) 큰 수에서 작은 수 빼기

--- SimpleThirdPractice/SimpleThirdPractice/ContentView10.swift
+++ SimpleThirdPractice/SimpleThirdPractice/ContentView10.swift
@@ -8,8 +8,107 @@
 import SwiftUI
 
 struct ContentView10: View {
+    @Environment(\.dismiss) private var dismiss
+    
+    @State private var number1: String = ""
+    @State private var number2: String = ""
+    @State private var result: String = ""
+    @FocusState private var isFocused: Bool
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            VStack {
+                Text("큰 수 빼기")
+                    .font(.custom("BazziRegular", size: 60))
+                    .padding(20)
+                
+                HStack{
+                    Image(systemName: "number.square")
+                        .resizable(resizingMode: .tile)
+                        .frame(width: 40.0, height: 40.0)
+                        .foregroundStyle(.black)
+                        .padding(.leading, 55)
+                        .padding(5)
+                    
+                    TextField("ex) 10", text: $number1)
+                        .modifier(StandardCustomFont())
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .keyboardType(.numberPad)
+                        .multilineTextAlignment(.leading)
+                        .border(.gray, width: 2)
+                        .cornerRadius(5.0)
+                        .padding(.trailing, 60)
+                        .focused($isFocused)
+                }
+                
+                HStack{
+                    Image(systemName: "number.square")
+                        .resizable(resizingMode: .tile)
+                        .frame(width: 40.0, height: 40.0)
+                        .foregroundStyle(.black)
+                        .padding(.leading, 55)
+                        .padding(5)
+                    
+                    TextField("ex) 30", text: $number2)
+                        .modifier(StandardCustomFont())
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .keyboardType(.numberPad)
+                        .multilineTextAlignment(.leading)
+                        .border(.gray, width: 2)
+                        .cornerRadius(5.0)
+                        .padding(.trailing, 60)
+                        .focused($isFocused)
+                }
+                
+                if number1 != "" && number2 != ""{
+                    Button(action: self.touchInSide, label: {
+                        Text("↓")
+                            .font(.custom("BazziRegular", size: 60))
+                            .modifier(StandardCustomFont())
+                            .multilineTextAlignment(.center)
+                    })
+                }
+                
+                if result != ""{
+                    Text("\(result)")
+                        .font(.custom("BazziRegular", size: 60))
+                        .modifier(StandardCustomFont())
+                        .padding(20)
+                }
+            }
+            .padding(.top, 200)
+            
+            GeometryReader{ geometry in
+                HStack{
+                    Button(action: {dismiss()}, label: {
+                        Text("이전 화면")
+                            .modifier(StandardCustomFont())
+                            .frame(width: geometry.size.width/2, height: geometry.size.height*5/3)
+                    })
+                    
+                    NavigationLink(destination: ContentView11(), label: {
+                        Text("다음 화면")
+                            .modifier(StandardCustomFont())
+                            .frame(width: geometry.size.width/2, height: geometry.size.height*5/3)
+                    })
+                }
+            }
+        }
+        .background(Image("skyGlass")
+            .resizable()
+            .aspectRatio(contentMode: .fill)
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .opacity(0.6)
+            .ignoresSafeArea())
+        .navigationBarBackButtonHidden()
+    }
+    
+    func touchInSide(){
+        guard let intNum1 = Int(self.number1) else { return }
+        guard let intNum2 = Int(self.number2) else { return }
+        
+        self.result = intNum1 > intNum2 ? "\(intNum1-intNum2)" : "\(intNum2-intNum1)"
+        isFocused = false
     }
 }
 
