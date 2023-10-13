9ea7df14be76f41546a11714f0ca214c9a5436d3
NohJooYoung <nohjoo97@naver.com>
2023-10-13 15:50:33 +0900

    10/13 first commit

---
+++ SimpleFourthPractice/SimpleFourthPractice/SimpleFourthPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleFourthPracticeApp.swift
+//  SimpleFourthPractice
+//
+//  Created by 노주영 on 2023/10/13.
+//
+
+import SwiftUI
+
+@main
+struct SimpleFourthPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
