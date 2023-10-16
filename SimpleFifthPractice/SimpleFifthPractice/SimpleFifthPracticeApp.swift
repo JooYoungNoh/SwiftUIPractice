0ce288fa54538d14fd2b08c4a95171a4f75d7aba
NohJooYoung <nohjoo97@naver.com>
2023-10-16 11:32:04 +0900

    10/16 실습 first commit

---
+++ SimpleFifthPractice/SimpleFifthPractice/SimpleFifthPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleFifthPracticeApp.swift
+//  SimpleFifthPractice
+//
+//  Created by 노주영 on 2023/10/16.
+//
+
+import SwiftUI
+
+@main
+struct SimpleFifthPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
