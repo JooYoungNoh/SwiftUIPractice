9e7c7e54c90573f55da9111222b99e1d8015f802
NohJooYoung <nohjoo97@naver.com>
2023-10-16 13:42:43 +0900

    UI 예제 2

--- SimpleFifthPractice/SimpleFifthPractice/UIExampleTwo.swift
+++ SimpleFifthPractice/SimpleFifthPractice/UIExampleTwo.swift
@@ -8,8 +8,46 @@
 import SwiftUI
 
 struct UIExampleTwo: View {
+    @Environment(\.dismiss) private var dismiss
+    
+    @State private var count: Int = 0
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            HStack{
+                Button(action: {dismiss()}, label: {
+                    Label("Back", systemImage: "arrowshape.backward.fill")
+                        .padding(5)
+                        .modifier(StandardFontCustom())
+                        .cornerRadius(5)
+                })
+                .padding(.leading, 10)
+                Spacer()
+            }
+            
+            Spacer()
+            Text("\(self.count)")
+                .font(.custom("BazziRegular", size: 25))
+                .foregroundStyle(.white)
+            
+            Spacer()
+            Button(action: self.touchInSide, label: {
+                Text("Count")
+                    .padding(10)
+                    .frame(maxWidth: .infinity, maxHeight: 50)
+                    .modifier(StandardFontCustom())
+                    .cornerRadius(5)
+            })
+            .padding(.horizontal, 10)
+            .padding(.bottom, 10)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.teal)
+        .navigationBarBackButtonHidden()
+    }
+    
+    func touchInSide(){
+        count += 1
     }
 }
 
