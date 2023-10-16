2f05630505e8b9416eb1b9d90da70bfbd800de91
NohJooYoung <nohjoo97@naver.com>
2023-10-16 13:44:08 +0900

    UI 예제 3 first commit

---
+++ SimpleFifthPractice/SimpleFifthPractice/UIExampleThree.swift
@@ -0,0 +1,18 @@
+//
+//  UIExampleThree.swift
+//  SimpleFifthPractice
+//
+//  Created by 노주영 on 2023/10/16.
+//
+
+import SwiftUI
+
+struct UIExampleThree: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UIExampleThree()
+}
