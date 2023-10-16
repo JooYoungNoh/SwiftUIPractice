fbdfd3b61c71dcb38ebd0d6f330325010fa574bb
NohJooYoung <nohjoo97@naver.com>
2023-10-16 14:10:14 +0900

    UI 예제 3

--- SimpleFifthPractice/SimpleFifthPractice/UIExampleThree.swift
+++ SimpleFifthPractice/SimpleFifthPractice/UIExampleThree.swift
@@ -8,8 +8,56 @@
 import SwiftUI
 
 struct UIExampleThree: View {
+    @Environment(\.dismiss) private var dismiss
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            HStack{
+                Button(action: {dismiss()}, label: {
+                    Label("Back", systemImage: "arrowshape.backward.fill")
+                        .padding(5)
+                        .modifier(StandardFontCustom())
+                        .cornerRadius(5)
+                })
+                .padding(.leading, 10)
+                Spacer()
+            }
+            
+            Button(action: {}, label: {
+                Text("Button")
+                    .padding(10)
+                    .frame(maxWidth: .infinity, maxHeight: 50)
+                    .modifier(StandardFontCustom())
+                    .cornerRadius(5)
+            })
+            .padding(.horizontal, 10)
+            .padding(.top, 10)
+            
+            Spacer()
+            Button(action: {}, label: {
+                Text("Button")
+                    .padding(10)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .modifier(StandardFontCustom())
+                    .cornerRadius(5)
+            })
+            .padding(.vertical, 40)
+            .padding(.horizontal, 10)
+            
+            Spacer()
+            Button(action: {}, label: {
+                Text("Button")
+                    .padding(10)
+                    .frame(maxWidth: .infinity, maxHeight: 50)
+                    .modifier(StandardFontCustom())
+                    .cornerRadius(5)
+            })
+            .padding(.horizontal, 10)
+            .padding(.bottom, 10)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.teal)
+        .navigationBarBackButtonHidden()
     }
 }
 
