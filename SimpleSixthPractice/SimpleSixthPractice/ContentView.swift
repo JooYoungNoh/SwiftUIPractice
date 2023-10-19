ceb4f93f3c093100e140e0e8930c5ea2ee84655d
NohJooYoung <nohjoo97@naver.com>
2023-10-19 14:31:53 +0900

    비동기 함수 표현

--- SimpleSixthPractice/SimpleSixthPractice/ContentView.swift
+++ SimpleSixthPractice/SimpleSixthPractice/ContentView.swift
@@ -10,19 +10,26 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Button(action: {doSomething()}, label: {
+            Button(action: {
+                //동기 함수(action부분이 메인 스레드에서 동작)에서 비동기 함수 호출(Task
+                Task {
+                    await doSomething()
+                }
+            }, label: {
                 Text("Do Something")
             })
         }
         .padding()
     }
     
-    func doSomething(){
+    //비동기 함수 선언 async
+    //비동기 함수 호출 await
+    func doSomething() async {
         print("Start \(Date())")
-        takeTooLong()
+        await takeTooLong()
         print("Start \(Date())")
     }
-    func takeTooLong(){
+    func takeTooLong() async {
         sleep(5)
         print("Async task completed at \(Date())")
     }
