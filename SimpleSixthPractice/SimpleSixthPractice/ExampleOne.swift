1a05ce488be2def60e6d55ac9f771d7709d31a6f
NohJooYoung <nohjoo97@naver.com>
2023-10-19 16:51:21 +0900

    비동기 예제 1 first commit

---
+++ SimpleSixthPractice/SimpleSixthPractice/ExampleOne.swift
@@ -0,0 +1,18 @@
+//
+//  ExampleOne.swift
+//  SimpleSixthPractice
+//
+//  Created by 노주영 on 2023/10/19.
+//
+
+import SwiftUI
+
+struct ExampleOne: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExampleOne()
+}
