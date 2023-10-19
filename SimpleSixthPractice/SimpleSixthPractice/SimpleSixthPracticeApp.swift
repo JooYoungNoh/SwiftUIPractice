e1838bf5174087061838f2831b3f42866ecb884e
NohJooYoung <nohjoo97@naver.com>
2023-10-19 13:55:02 +0900

    동기 비동기 연습 first commit

---
+++ SimpleSixthPractice/SimpleSixthPractice/SimpleSixthPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleSixthPracticeApp.swift
+//  SimpleSixthPractice
+//
+//  Created by 노주영 on 2023/10/19.
+//
+
+import SwiftUI
+
+@main
+struct SimpleSixthPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
