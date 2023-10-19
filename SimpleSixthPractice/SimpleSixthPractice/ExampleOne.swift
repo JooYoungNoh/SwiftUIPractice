66f336958da85eed6d1ede011dfae57111b3ee23
NohJooYoung <nohjoo97@naver.com>
2023-10-19 18:30:35 +0900

    withTaskGroup을 사용한 덧샘

--- SimpleSixthPractice/SimpleSixthPractice/ExampleOne.swift
+++ SimpleSixthPractice/SimpleSixthPractice/ExampleOne.swift
@@ -12,9 +12,8 @@ struct ExampleOne: View {
     var body: some View {
         VStack{
             Button(action: {
-                //동기 함수(action부분이 메인 스레드에서 동작)에서 비동기 함수 호출(Task
                 Task {
-                    result = await sum(start: 1, end: 100)
+                   result = await sum(start: 1, end: 100)
                 }
             }, label: {
                 Text("Do Sum")
@@ -24,14 +23,34 @@ struct ExampleOne: View {
                 .padding(.top, 30)
         }
     }
-    func sum(start: Int, end: Int) async -> Int {
-        async let a = sumSub(start, end/2)
-        async let b = sumSub(end/2+1, end)
-        return await a + b
+    //비동기로 처리하는 함수
+    func sum(start: Int, end: Int) async -> Int{
+        var sumNumber: [Int: Int] = [:]
+        var sum: Int = 0
+        let arr: [Int] = [0, 50]
+        
+        await withTaskGroup(of: (Int, Int).self){ group in
+            for i in 0...1{
+                group.addTask{
+                    return(i, await sumSub(arr[i]+1, arr[i]+50))
+                }
+            }
+            
+            for await (task, result) in group {
+                sumNumber.updateValue(result, forKey: task)
+            }
+            
+            for (key, value) in sumNumber{
+                sum += sumNumber[key] ?? 0
+                print("\(key): \(value)")
+            }
+            print("결과: \(sum)")
+        }
+        return sum
     }
-    
+    //더하는 함수
     func sumSub(_ start: Int, _ end: Int) async -> Int {
-        sleep(1)
+        sleep(2)
         var sum = 0
         (start...end).forEach{
             sum += $0
--- SimpleSixthPractice/SimpleSixthPractice/SimpleSixthPracticeApp.swift
+++ SimpleSixthPractice/SimpleSixthPractice/SimpleSixthPracticeApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SimpleSixthPracticeApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ExampleOne()
         }
     }
 }
