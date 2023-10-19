e9cbac21bedc4017368b9f67802ed7f406f32460
NohJooYoung <nohjoo97@naver.com>
2023-10-19 16:32:44 +0900

    데이터 경쟁 피하기1

--- SimpleSixthPractice/SimpleSixthPractice/ContentView.swift
+++ SimpleSixthPractice/SimpleSixthPractice/ContentView.swift
@@ -34,15 +34,30 @@ struct ContentView: View {
     // cancelAll(): 그룹의 모든 작업을 취소하는 메서드
     // isCancelled: 작업 그룹이 이미 취소되었는지 여부를 확인하는 속성
     // isEmpty:     작업 그룹 내에 작업이 남아 있는지 여부를 확이하는 속성
+    // 데이터 경쟁 피하기
+    // 동시에 데이터에 접근하는 여러 작업은 데이터 경쟁(Data race) 조건이 발생할 위험이 있음
+    // 여러 작업이 동시에 동일한 데이터에 접근하고 시도하면 데이터 오류가 발생
     func doSomething() async {
+        var timeStamps: [Int: Date] = [:]
         print("Start \(Date())")
         
-        await withTaskGroup(of: Void.self) { group in
+        // 반환 타입을 변경 : Void.self -> (Int, Date)
+        await withTaskGroup(of: (Int, Date).self) { group in
             for i in 1...5 {
                 group.addTask {
-                    let result = await takeTooLong()
-                    print("Completed Task \(i) = \(result)")
+                    return (i, await takeTooLong())
                 }
+            }
+            // for-await 표현식을 사용하여 비동기적으로 반환되는 일련의 값을 루프
+            // 동시 작업에서 반환되는 값의 수신을 기다려서 처리
+            // 일련의 데이터가 AsyncSequence 프로토콜 준수가 필수 요구사항
+            for await (task, date) in group {
+                timeStamps.updateValue(date, forKey: task)
+            }
+            
+            // 작업 그룹이 종료된 후 저장된 timeStamps 딕셔너리 항목을 출력
+            for (task, date) in timeStamps {
+                print("Task: \(task), date: \(date)")
             }
         }
     }
