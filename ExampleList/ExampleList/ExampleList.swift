c1e2c0db4a3875b9f347e6aefec399f8d7c097bb
NohJooYoung <nohjoo97@naver.com>
2023-10-20 12:52:13 +0900

    실습 예제 리스트 first commit

---
+++ ExampleList/ExampleList/ExampleList.swift
@@ -0,0 +1,18 @@
+//
+//  ExampleList.swift
+//  ExampleList
+//
+//  Created by 노주영 on 2023/10/20.
+//
+
+import SwiftUI
+
+struct ExampleList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExampleList()
+}
---
+++ ExampleList/ExampleList/ExampleListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleListApp.swift
+//  ExampleList
+//
+//  Created by 노주영 on 2023/10/20.
+//
+
+import SwiftUI
+
+@main
+struct ExampleListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ExampleList()
+        }
+    }
+}
