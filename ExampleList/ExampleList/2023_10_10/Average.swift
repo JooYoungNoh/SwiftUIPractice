a1839fca6a79384fd60ea600d78c79df7a1b1440
NohJooYoung <nohjoo97@naver.com>
2023-10-20 18:04:31 +0900

    2023_10_10실습 옮기기 first commit

---
+++ ExampleList/ExampleList/2023_10_10/Average.swift
@@ -0,0 +1,18 @@
+//
+//  Average.swift
+//  ExampleList
+//
+//  Created by 노주영 on 2023/10/20.
+//
+
+import SwiftUI
+
+struct Average: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Average()
+}
---
+++ ExampleList/ExampleList/2023_10_10/FourBasicOperations.swift
@@ -0,0 +1,18 @@
+//
+//  FourBasicOperations.swift
+//  ExampleList
+//
+//  Created by 노주영 on 2023/10/20.
+//
+
+import SwiftUI
+
+struct FourBasicOperations: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FourBasicOperations()
+}
