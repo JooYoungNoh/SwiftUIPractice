fad866291dfa84eba9d51e211b3e79d7aec0245a
NohJooYoung <nohjoo97@naver.com>
2023-10-20 13:12:04 +0900

    폰트 ViewModifier 파일 생성

---
+++ ExampleList/ExampleList/FontViewModifier.swift
@@ -0,0 +1,24 @@
+//
+//  FontViewModifier.swift
+//  ExampleList
+//
+//  Created by 노주영 on 2023/10/20.
+//
+
+import SwiftUI
+
+struct StandardCustomFontTitle: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.custom("MaplestoryOTFBold", size: 40))
+            .foregroundColor(.black)
+    }
+}
+
+struct StandardCustomFontText: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.custom("MaplestoryOTFLight", size: 20))
+            .foregroundColor(.black)
+    }
+}
