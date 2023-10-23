f343e99e2ea797db7f9fe92f0b584fe1bfea3bec
NohJooYoung <nohjoo97@naver.com>
2023-10-23 13:40:18 +0900

    시급 계산기 실습 옮겨오기 및 코드 리펙토링

--- ExampleList/ExampleList/2023_10_11/HourlyRate.swift
+++ ExampleList/ExampleList/2023_10_11/HourlyRate.swift
@@ -8,10 +8,102 @@
 import SwiftUI
 
 struct HourlyRate: View {
+    @Environment(\.dismiss) private var dismiss
+    @FocusState private var isFocused: Bool
+    
+    @State private var time: String = ""
+    @State private var money: String = ""
+    @State private var result: String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                Button(action: {dismiss()}, label: {
+                    Text("< Back")
+                        .font(.custom("MaplestoryOTFLight", size: 25))
+                        .foregroundStyle(.black)
+                        .bold()
+                        
+                })
+                Spacer()
+            }
+            .padding(.horizontal, 20)
+            
+            Spacer()
+            VStack {
+                Text("시급 계산기")
+                    .modifier(StandardCustomFontTitle())
+                    .padding(20)
+                
+                HStack{
+                    Image(systemName: "clock")
+                        .resizable(resizingMode: .tile)
+                        .frame(width: 40.0, height: 40.0)
+                        .foregroundStyle(.black)
+                        .padding(.trailing, 10)
+                    
+                    TextField("시간", text: $time)
+                        .modifier(StandardCustomFontText())
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .keyboardType(.numberPad)
+                        .multilineTextAlignment(.leading)
+                        .border(.gray, width: 2)
+                        .cornerRadius(5.0)
+                        .focused($isFocused)
+                }
+                .padding(20)
+                
+                HStack{
+                    Image(systemName: "dollarsign.circle")
+                        .resizable(resizingMode: .tile)
+                        .frame(width: 40.0, height: 40.0)
+                        .foregroundStyle(.black)
+                        .padding(.trailing, 10)
+                    
+                    TextField("시급", text: $money)
+                        .modifier(StandardCustomFontText())
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .keyboardType(.numberPad)
+                        .multilineTextAlignment(.leading)
+                        .border(.gray, width: 2)
+                        .cornerRadius(5.0)
+                        .focused($isFocused)
+                }
+                .padding(.horizontal, 20)
+                
+                if time != "" && money != "" {
+                    Button(action: self.touchInSide, label: {
+                        Text("↓")
+                            .font(.custom("MaplestoryOTFLight", size: 50))
+                            .foregroundStyle(.black)
+                            .multilineTextAlignment(.center)
+                    })
+                    .padding()
+                }
+                
+                Text("\(result)")
+                    .modifier(StandardCustomFontTitle())
+                    .multilineTextAlignment(.center)
+                    .padding(20)
+            }
+            Spacer()
+        }
+        .navigationBarBackButtonHidden()
+    }
+    //화살표 버튼 메서드
+    func touchInSide(){
+        guard let changeTime = Double(self.time) else { return }
+        guard let changeMoney = Double(self.money) else { return }
+        
+        if changeTime > 8 {
+            self.result = "총 급여 :  \(Int(changeMoney*8 + changeMoney*(changeTime-8)*1.5))"
+        } else {
+            self.result = "총 급여 :  \(Int(changeMoney*changeTime))"
+        }
+        isFocused = false
     }
 }
+
 
 #Preview {
     HourlyRate()
