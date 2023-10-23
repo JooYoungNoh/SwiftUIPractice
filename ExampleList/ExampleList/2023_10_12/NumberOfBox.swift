e4754bec1095fd221f5076487ec3dcb94005adaa
NohJooYoung <nohjoo97@naver.com>
2023-10-23 15:26:35 +0900

    상자의 개수 구하기 실습 옮겨오기 및 코드 리펙토링

--- ExampleList/ExampleList/2023_10_12/NumberOfBox.swift
+++ ExampleList/ExampleList/2023_10_12/NumberOfBox.swift
@@ -8,8 +8,94 @@
 import SwiftUI
 
 struct NumberOfBox: View {
+    @Environment(\.dismiss) private var dismiss
+    @FocusState private var isFocused: Bool
+    
+    @State private var ball: String = ""
+    @State private var box: String = ""
+    @State private var result: String = "대기중..."
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                Button(action: {dismiss()}, label: {
+                    Text("< Back")
+                        .font(.custom("MaplestoryOTFLight", size: 25))
+                        .foregroundStyle(.black)
+                        .bold()
+                    
+                })
+                Spacer()
+            }
+            .padding(.horizontal, 20)
+            
+            Spacer()
+            VStack {
+                Text("상자의 개수")
+                    .modifier(StandardCustomFontTitle())
+                    .padding(20)
+                
+                HStack{
+                    Image(systemName: "basketball")
+                        .resizable(resizingMode: .tile)
+                        .frame(width: 40.0, height: 40.0)
+                        .foregroundStyle(.black)
+                        .padding(.trailing, 10)
+                    
+                    TextField("농구공의 개수", text: $ball)
+                        .font(.custom("BazziRegular", size: 25))
+                        .modifier(StandardCustomFontText())
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .keyboardType(.numberPad)
+                        .multilineTextAlignment(.leading)
+                        .border(.gray, width: 2)
+                        .cornerRadius(5.0)
+                        .focused($isFocused)
+                }
+                .padding(20)
+                
+                HStack{
+                    Image(systemName: "shippingbox")
+                        .resizable(resizingMode: .tile)
+                        .frame(width: 40.0, height: 40.0)
+                        .foregroundStyle(.black)
+                        .padding(.trailing, 10)
+                    
+                    TextField("한 상자 넣을 공의 개수", text: $box)
+                        .font(.custom("BazziRegular", size: 25))
+                        .modifier(StandardCustomFontText())
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .keyboardType(.numberPad)
+                        .multilineTextAlignment(.leading)
+                        .border(.gray, width: 2)
+                        .cornerRadius(5.0)
+                        .focused($isFocused)
+                }
+                .padding(20)
+                
+                Button(action: self.touchInSide, label: {
+                    Text("↓")
+                        .font(.custom("MaplestoryOTFLight", size: 50))
+                        .foregroundStyle(.black)
+                        .multilineTextAlignment(.center)
+                })
+                .padding()
+                
+                Text("\(result)")
+                    .modifier(StandardCustomFontTitle())
+                    .padding(20)
+            }
+            Spacer()
+        }
+        .navigationBarBackButtonHidden()
+    }
+    
+    func touchInSide(){
+        isFocused = false
+        guard let intball = Int(self.ball) else { return }
+        guard let intbox = Int(self.box) else { return }
+        
+        self.result = intball%intbox == 0 ? "\(intball/intbox)개" : "\(intball/intbox+1)개"
     }
 }
 
