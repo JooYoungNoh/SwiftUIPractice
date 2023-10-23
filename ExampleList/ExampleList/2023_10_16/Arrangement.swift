5f02f29f3381dcc201bdbf56a42f3f44ed42e8b0
NohJooYoung <nohjoo97@naver.com>
2023-10-23 16:05:43 +0900

    2023_10_16 실습 옮기기 first commit

---
+++ ExampleList/ExampleList/2023_10_16/Arrangement.swift
@@ -0,0 +1,18 @@
+//
+//  Arrangement.swift
+//  ExampleList
+//
+//  Created by 노주영 on 2023/10/23.
+//
+
+import SwiftUI
+
+struct Arrangement: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Arrangement()
+}
---
+++ ExampleList/ExampleList/2023_10_16/Counter.swift
@@ -0,0 +1,18 @@
+//
+//  Counter.swift
+//  ExampleList
+//
+//  Created by 노주영 on 2023/10/23.
+//
+
+import SwiftUI
+
+struct Counter: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Counter()
+}
