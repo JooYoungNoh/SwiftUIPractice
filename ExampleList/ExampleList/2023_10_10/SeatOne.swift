72ddc4c9f261e46554ce8b3814f2c90eb6141128
NohJooYoung <nohjoo97@naver.com>
2023-10-23 09:40:49 +0900

    일의 자리 1로 바꾸기 실습 옮기기 및 코드 리펙토링

--- ExampleList/ExampleList/2023_10_10/SeatOne.swift
+++ ExampleList/ExampleList/2023_10_10/SeatOne.swift
@@ -8,8 +8,65 @@
 import SwiftUI
 
 struct SeatOne: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    @Environment(\.dismiss) private var dismiss
+    @FocusState private var isFocused: Bool
+    
+    @State private var number1: String = ""
+    @State private var result: String = ""
+    
+    var body: some View{
+        VStack{
+            HStack {
+                Button(action: {dismiss()}, label: {
+                    Text("< Back")
+                        .font(.custom("MaplestoryOTFLight", size: 25))
+                        .foregroundStyle(.black)
+                        .bold()
+                        
+                })
+                Spacer()
+            }
+            .padding(.horizontal, 20)
+            
+            Spacer()
+            VStack {
+                Text("일의 자리 1로 바꾸기")
+                    .modifier(StandardCustomFontTitle())
+                    .multilineTextAlignment(.center)
+                
+                TextField("ex) 456", text: $number1)
+                    .modifier(StandardCustomFontText())
+                    .multilineTextAlignment(.leading)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .border(.black, width: 2)
+                    .keyboardType(.numberPad)
+                    .focused($isFocused)
+                    .padding(.horizontal, 35)
+                    .padding(.vertical, 20)
+                
+                Button(action: self.touchInSide, label: {
+                    if let _ = Int(self.number1){
+                        Text("↓")
+                            .font(.custom("MaplestoryOTFLight", size: 50))
+                            .foregroundStyle(.black)
+                            .multilineTextAlignment(.center)
+                    }
+                })
+                
+                Text(result)
+                    .modifier(StandardCustomFontTitle())
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 35)
+                    .padding(.vertical, 20)
+            }
+            
+            Spacer()
+        }
+        .navigationBarBackButtonHidden()
+    }
+    func touchInSide(){
+        self.result = String(Int(self.number1)!/10*10+1)
+        self.number1 = ""
     }
 }
 
