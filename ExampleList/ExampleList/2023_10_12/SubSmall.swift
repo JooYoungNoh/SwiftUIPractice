f6c035869e417ed4744f582c8cf6aef03bd19cd3
NohJooYoung <nohjoo97@naver.com>
2023-10-23 14:34:01 +0900

    큰수에서 작은수 빼기 실습 옮겨오기 및 코드 리펙토링

--- ExampleList/ExampleList/2023_10_12/SubSmall.swift
+++ ExampleList/ExampleList/2023_10_12/SubSmall.swift
@@ -8,8 +8,92 @@
 import SwiftUI
 
 struct SubSmall: View {
+    @Environment(\.dismiss) private var dismiss
+    @FocusState private var isFocused: Bool
+    
+    @State private var number1: String = ""
+    @State private var number2: String = ""
+    @State private var result: String = "대기중..."
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                Button(action: {dismiss()}, label: {
+                    Text("< Back")
+                        .font(.custom("MaplestoryOTFLight", size: 25))
+                        .foregroundStyle(.black)
+                        .bold()
+                    
+                })
+                Spacer()
+            }
+            .padding(.horizontal, 20)
+            
+            Spacer()
+            VStack {
+                Text("큰 수 빼기")
+                    .modifier(StandardCustomFontTitle())
+                    .padding(20)
+                
+                HStack{
+                    Image(systemName: "number.square")
+                        .resizable(resizingMode: .tile)
+                        .frame(width: 40.0, height: 40.0)
+                        .foregroundStyle(.black)
+                        .padding(.trailing, 10)
+                    
+                    TextField("ex) 10", text: $number1)
+                        .modifier(StandardCustomFontText())
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .keyboardType(.numberPad)
+                        .multilineTextAlignment(.leading)
+                        .border(.gray, width: 2)
+                        .cornerRadius(5.0)
+                        .focused($isFocused)
+                }
+                .padding(20)
+                
+                HStack{
+                    Image(systemName: "number.square")
+                        .resizable(resizingMode: .tile)
+                        .frame(width: 40.0, height: 40.0)
+                        .foregroundStyle(.black)
+                        .padding(.trailing, 10)
+                    
+                    TextField("ex) 30", text: $number2)
+                        .modifier(StandardCustomFontText())
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .keyboardType(.numberPad)
+                        .multilineTextAlignment(.leading)
+                        .border(.gray, width: 2)
+                        .cornerRadius(5.0)
+                        .focused($isFocused)
+                }
+                .padding(.horizontal, 20)
+                
+                Button(action: self.touchInSide, label: {
+                    Text("↓")
+                        .font(.custom("MaplestoryOTFLight", size: 50))
+                        .foregroundStyle(.black)
+                        .multilineTextAlignment(.center)
+                })
+                .padding()
+                
+                Text("\(result)")
+                    .modifier(StandardCustomFontTitle())
+                    .padding(20)
+            }
+            Spacer()
+        }
+        .navigationBarBackButtonHidden()
+    }
+    
+    func touchInSide(){
+        isFocused = false
+        guard let intNum1 = Int(self.number1) else { return }
+        guard let intNum2 = Int(self.number2) else { return }
+        
+        self.result = intNum1 > intNum2 ? "\(intNum1-intNum2)" : "\(intNum2-intNum1)"
     }
 }
 
