fcef87ccbdbc8851287b738352de9bf692c9b016
NohJooYoung <nohjoo97@naver.com>
2023-10-23 13:17:31 +0900

    초 나누기 실습 옮겨오기 및 코드 리펙토링

--- ExampleList/ExampleList/2023_10_11/DivideSeconds.swift
+++ ExampleList/ExampleList/2023_10_11/DivideSeconds.swift
@@ -8,8 +8,80 @@
 import SwiftUI
 
 struct DivideSeconds: View {
+    @Environment(\.dismiss) private var dismiss
+    @FocusState private var isFocused: Bool
+    
+    @State private var number: String = ""
+    @State private var result: String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+       VStack {
+           HStack {
+               Button(action: {dismiss()}, label: {
+                   Text("< Back")
+                       .font(.custom("MaplestoryOTFLight", size: 25))
+                       .foregroundStyle(.black)
+                       .bold()
+                       
+               })
+               Spacer()
+           }
+           .padding(.horizontal, 20)
+           
+           Spacer()
+           VStack {
+               Text("초 나누기")
+                   .modifier(StandardCustomFontTitle())
+                   .padding(20)
+               
+               HStack{
+                   Image(systemName: "clock")
+                       .resizable(resizingMode: .tile)
+                       .frame(width: 40.0, height: 40.0)
+                       .foregroundStyle(.black)
+                       .padding(.trailing, 10)
+                   
+                   TextField("초 입력", text: $number)
+                       .modifier(StandardCustomFontText())
+                       .textFieldStyle(RoundedBorderTextFieldStyle())
+                       .keyboardType(.numberPad)
+                       .multilineTextAlignment(.leading)
+                       .border(.gray, width: 2)
+                       .cornerRadius(5.0)
+                       .focused($isFocused)
+               }
+               .padding(20)
+               
+               if number != "" {
+                   Button(action: self.touchInSide, label: {
+                       Text("↓")
+                           .font(.custom("MaplestoryOTFLight", size: 50))
+                           .foregroundStyle(.black)
+                           .multilineTextAlignment(.center)
+                   })
+               }
+               
+               Text("\(result)")
+                   .modifier(StandardCustomFontTitle())
+                   .padding(20)
+           }
+           Spacer()
+       }
+       .navigationBarBackButtonHidden()
+    }
+    //화살표 버튼 메서드
+    func touchInSide(){
+        if let changeNumber = Int(self.number){
+            let hour: String = "\(changeNumber/3600)"
+            let minute: String = "\(changeNumber%3600/60)"
+            let sec: String = "\(changeNumber%3600%60)"
+            
+            self.result = hour + "시 " + minute + "분 " + sec + "초"
+            
+            isFocused = false
+        } else {
+            self.result = ""
+        }
     }
 }
 
