ae37fc9bbc51615e5acc7abdf5cc63926ed64432
NohJooYoung <nohjoo97@naver.com>
2023-10-23 10:00:48 +0900

    평균 구하기 실습 옮기기 및 코드 리펙토링

--- ExampleList/ExampleList/2023_10_10/Average.swift
+++ ExampleList/ExampleList/2023_10_10/Average.swift
@@ -8,10 +8,124 @@
 import SwiftUI
 
 struct Average: View {
+    @Environment(\.dismiss) private var dismiss
+    @FocusState private var isFocused: Bool
+    
+    @State private var swiftNum: String = ""
+    @State private var iosNum: String = ""
+    @State private var webNum: String = ""
+    
+    @State private var result: String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            HStack {
+                Button(action: {dismiss()}, label: {
+                    Text("< Back")
+                        .font(.custom("MaplestoryOTFLight", size: 25))
+                        .foregroundStyle(.black)
+                        .bold()
+                        
+                })
+                Spacer()
+            }
+            .padding(.horizontal, 20)
+            
+            Spacer()
+            
+            VStack {
+                Text("평균 구하기")
+                    .modifier(StandardCustomFontTitle())
+                    .multilineTextAlignment(.center)
+                    .padding(.bottom, 20)
+                
+                HStack{
+                    Text("Swift")
+                        .font(.custom("MaplestoryOTFLight", size: 25))
+                        .bold()
+                        .frame(width: 80, height: 30)
+                    
+                    TextField("점수", text: $swiftNum)
+                        .modifier(StandardCustomFontText())
+                        .multilineTextAlignment(.leading)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .border(.gray, width: 2)
+                        .cornerRadius(5.0)
+                        .keyboardType(.numberPad)
+                        .focused($isFocused)
+                }
+                .padding(.horizontal, 20)
+                .padding(.vertical, 10)
+                
+                HStack{
+                    Text("iOS")
+                        .font(.custom("MaplestoryOTFLight", size: 25))
+                        .bold()
+                        .frame(width: 80, height: 30)
+                    
+                    TextField("점수", text: $iosNum)
+                        .modifier(StandardCustomFontText())
+                        .multilineTextAlignment(.leading)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .border(.gray, width: 2)
+                        .cornerRadius(5.0)
+                        .keyboardType(.numberPad)
+                        .focused($isFocused)
+                }
+                .padding(.horizontal, 20)
+                .padding(.vertical, 10)
+                
+                HStack{
+                    Text("Web")
+                        .font(.custom("MaplestoryOTFLight", size: 25))
+                        .bold()
+                        .frame(width: 80, height: 30)
+                    
+                    TextField("점수", text: $webNum)
+                        .modifier(StandardCustomFontText())
+                        .multilineTextAlignment(.leading)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .border(.gray, width: 2)
+                        .cornerRadius(5.0)
+                        .keyboardType(.numberPad)
+                        .focused($isFocused)
+                }
+                .padding(.horizontal, 20)
+                .padding(.vertical, 10)
+                
+                Button(action: self.touchInSide, label: {
+                    if self.swiftNum != "" && self.iosNum != "" && self.webNum != "" {
+                        Text("↓")
+                            .font(.custom("MaplestoryOTFLight", size: 50))
+                            .foregroundStyle(.black)
+                            .multilineTextAlignment(.center)
+                    }
+                })
+                
+                Text(result)
+                    .modifier(StandardCustomFontTitle())
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 30)
+            }
+            Spacer()
+        }
+    }
+    func touchInSide(){
+        isFocused = false
+        guard let changeSwift = Double(self.swiftNum) else { return }
+        guard let changeiOS = Double(self.iosNum) else { return }
+        guard let changeWeb = Double(self.webNum) else { return }
+        let add = changeSwift+changeiOS+changeWeb
+        
+        if add.truncatingRemainder(dividingBy: 3.0) == 0 {
+            self.result = "\(Int(add/3))"
+        } else {
+            self.result = String(format: "%.2f", add/3)
+        }
     }
 }
+
 
 #Preview {
     Average()
