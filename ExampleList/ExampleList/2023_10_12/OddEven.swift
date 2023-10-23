3ff82e3b227212969f807389690922ced8a3f7eb
NohJooYoung <nohjoo97@naver.com>
2023-10-23 14:08:32 +0900

    2023_10_12 실습 옮기기 first commit

---
+++ ExampleList/ExampleList/2023_10_12/OddEven.swift
@@ -0,0 +1,18 @@
+//
+//  OddEven.swift
+//  ExampleList
+//
+//  Created by 노주영 on 2023/10/23.
+//
+
+import SwiftUI
+
+struct OddEven: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OddEven()
+}
