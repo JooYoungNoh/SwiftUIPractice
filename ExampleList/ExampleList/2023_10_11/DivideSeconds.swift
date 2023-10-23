79797584177ea4d81fce4215a0ce2fe784898af9
NohJooYoung <nohjoo97@naver.com>
2023-10-23 13:00:45 +0900

    2023_10_11 실습 옮기기 first commit

---
+++ ExampleList/ExampleList/2023_10_11/DivideSeconds.swift
@@ -0,0 +1,18 @@
+//
+//  DivideSeconds.swift
+//  ExampleList
+//
+//  Created by 노주영 on 2023/10/23.
+//
+
+import SwiftUI
+
+struct DivideSeconds: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DivideSeconds()
+}
---
+++ ExampleList/ExampleList/2023_10_11/HourlyRate.swift
@@ -0,0 +1,18 @@
+//
+//  HourlyRate.swift
+//  ExampleList
+//
+//  Created by 노주영 on 2023/10/23.
+//
+
+import SwiftUI
+
+struct HourlyRate: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HourlyRate()
+}
