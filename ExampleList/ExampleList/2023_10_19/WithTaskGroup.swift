d0de6d2745a2cb30582d7cad3602e6fca5de474c
NohJooYoung <nohjoo97@naver.com>
2023-10-24 09:24:22 +0900

    withTaskGroup을 사용한 비동기 실습

--- ExampleList/ExampleList/2023_10_19/WithTaskGroup.swift
+++ ExampleList/ExampleList/2023_10_19/WithTaskGroup.swift
@@ -8,8 +8,78 @@
 import SwiftUI
 
 struct WithTaskGroup: View {
+    @Environment(\.dismiss) private var dismiss
+    
+    @State private var result: Int = 0
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            HStack {
+                Button(action: {dismiss()}, label: {
+                    Text("< Back")
+                        .font(.custom("MaplestoryOTFLight", size: 25))
+                        .foregroundStyle(.black)
+                        .bold()
+                    
+                })
+                Spacer()
+            }
+            .padding(.horizontal, 20)
+            
+            Spacer()
+            VStack {
+                Button(action: {
+                    Task {
+                       result = await sum(start: 1, end: 100)
+                    }
+                }, label: {
+                    Text("Do Sum")
+                        .modifier(StandardCustomFontTitle())
+                })
+                .padding(20)
+                
+                Text("\(result)")
+                    .modifier(StandardCustomFontTitle())
+                    .padding(20)
+                   
+            }
+            Spacer()
+        }
+        .navigationBarBackButtonHidden()
+    }
+    //비동기로 처리하는 함수
+    func sum(start: Int, end: Int) async -> Int{
+        var sumNumber: [Int: Int] = [:]
+        var sum: Int = 0
+        let arr: [Int] = [0, 50]
+        
+        await withTaskGroup(of: (Int, Int).self){ group in
+            for i in 0...1{
+                group.addTask{
+                    return(i, await sumSub(arr[i]+1, arr[i]+50))
+                }
+            }
+            
+            for await (task, result) in group {
+                sumNumber.updateValue(result, forKey: task)
+            }
+            
+            for (key, value) in sumNumber{
+                sum += sumNumber[key] ?? 0
+                print("\(key): \(value)")
+            }
+            print("결과: \(sum)")
+        }
+        return sum
+    }
+    //더하는 함수
+    func sumSub(_ start: Int, _ end: Int) async -> Int {
+        sleep(2)
+        var sum = 0
+        (start...end).forEach{
+            sum += $0
+        }
+        return sum
     }
 }
 
