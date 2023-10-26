06a4f17f3057688ff1c6923db2fb68d14a58b69a
NohJooYoung <nohjoo97@naver.com>
2023-10-26 13:11:46 +0900

    탭 뷰 생성

--- ExampleList/ExampleList/2023_10_26/Lifecycle.swift
+++ ExampleList/ExampleList/2023_10_26/Lifecycle.swift
@@ -8,8 +8,38 @@
 import SwiftUI
 
 struct Lifecycle: View {
+    @Environment(\.dismiss) private var dismiss
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                Button(action: {dismiss()}, label: {
+                    Text("< Back")
+                        .font(.custom("MaplestoryOTFLight", size: 25))
+                        .foregroundStyle(.black)
+                        .bold()
+                    
+                })
+                Spacer()
+            }
+            .padding(.horizontal, 20)
+            
+            Spacer()
+            
+            TabView {
+                FirstTabView()
+                    .tabItem {
+                        Image(systemName: "01.circle")
+                        Text("First")
+                    }
+                
+                SecondTabView()
+                    .tabItem {
+                        Image(systemName: "02.circle")
+                        Text("Second")
+                    }
+            }
+        }
     }
 }
 
