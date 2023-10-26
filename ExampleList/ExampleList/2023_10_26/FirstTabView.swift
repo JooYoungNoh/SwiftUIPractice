6ac0944422a988cac80c7e470a975e82f327eab2
NohJooYoung <nohjoo97@naver.com>
2023-10-26 13:58:42 +0900

    뷰를 만들 때의 비동기 작업

--- ExampleList/ExampleList/2023_10_26/FirstTabView.swift
+++ ExampleList/ExampleList/2023_10_26/FirstTabView.swift
@@ -5,19 +5,36 @@
 //  Created by 노주영 on 2023/10/26.
 //
 
+/*
+ 
+ - onAppear() 수정자   : 뷰가 나타날 때 실행
+ - onDisapper() 수정자 : 뷰가 사라질 때 실행
+ - task() 수정자       : 뷰를 만들때 비동기 작업을 시작
+ 
+*/
+
 import SwiftUI
 
 struct FirstTabView: View {
+    @State var title: String = "View One"
+    
     var body: some View {
         VStack {
-            Text("View One")
+            Text(title)
                 .onAppear(perform: {
                     print("onAppear triggered")
                 })
                 .onDisappear(perform: {
                     print("onDisappeared triggered")
                 })
+                .task(priority: TaskPriority.background) {
+                    title = await changeTitle()
+                }
         }
+    }
+    func changeTitle() async -> String {
+        sleep(5)
+        return "비동기 처리 완료"
     }
 }
 
