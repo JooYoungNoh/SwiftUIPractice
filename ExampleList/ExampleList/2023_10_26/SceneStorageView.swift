1569292267202f87e848c8530c4152b76d213426
NohJooYoung <nohjoo97@naver.com>
2023-10-26 15:50:23 +0900

    데이터 지속성 탭 뷰 생성

---
+++ ExampleList/ExampleList/2023_10_26/SceneStorageView.swift
@@ -0,0 +1,18 @@
+//
+//  SceneStorageView.swift
+//  ExampleList
+//
+//  Created by 노주영 on 2023/10/26.
+//
+
+import SwiftUI
+
+struct SceneStorageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SceneStorageView()
+}
