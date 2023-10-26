402a9088b4321f08a3a463a13a1d32faca36c843
NohJooYoung <nohjoo97@naver.com>
2023-10-26 16:09:26 +0900

    SceneStorage 사용

--- ExampleList/ExampleList/2023_10_26/SceneStorageView.swift
+++ ExampleList/ExampleList/2023_10_26/SceneStorageView.swift
@@ -8,8 +8,14 @@
 import SwiftUI
 
 struct SceneStorageView: View {
+    @SceneStorage("mytext") private var editorText: String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            TextEditor(text: $editorText)
+                .padding(30)
+        }
+        .modifier(StandardCustomFontText())
     }
 }
 
