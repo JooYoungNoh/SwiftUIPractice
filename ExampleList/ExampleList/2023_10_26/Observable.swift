972b92fab8db41c9689a4814b7ca40b8e13c64a1
NohJooYoung <nohjoo97@naver.com>
2023-10-26 14:35:12 +0900

    Observable 객체를 이용한 시간 카운터

--- ExampleList/ExampleList/2023_10_26/Observable.swift
+++ ExampleList/ExampleList/2023_10_26/Observable.swift
@@ -8,8 +8,42 @@
 import SwiftUI
 
 struct Observable: View {
+    @Environment(\.dismiss) private var dismiss
+    
+    @StateObject private var vm: ObservableVM = ObservableVM()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                Button(action: {dismiss()}, label: {
+                    Text("< Back")
+                        .font(.custom("MaplestoryOTFLight", size: 25))
+                        .foregroundStyle(.black)
+                        .bold()
+                    
+                })
+                Spacer()
+            }
+            .padding(.horizontal, 20)
+            
+            Spacer()
+            VStack {
+                Text("시간 카운터 = \(vm.timeCount)")
+                
+                Button(action: vm.resetCount, label: {
+                    Text("초기화")
+                        .padding()
+                })
+                .border(.black, width: 5)
+                .cornerRadius(3)
+                .padding(.top, 20)
+            }
+            .modifier(StandardCustomFontTitle())
+            .padding()
+            
+            Spacer()
+        }
+        .navigationBarBackButtonHidden()
     }
 }
 
