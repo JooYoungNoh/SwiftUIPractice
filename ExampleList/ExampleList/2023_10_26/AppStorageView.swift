a4723c7e511765b9e55eb924ae202a9163222583
NohJooYoung <nohjoo97@naver.com>
2023-10-26 16:18:30 +0900

    AppStorage 사용

--- ExampleList/ExampleList/2023_10_26/AppStorageView.swift
+++ ExampleList/ExampleList/2023_10_26/AppStorageView.swift
@@ -8,8 +8,14 @@
 import SwiftUI
 
 struct AppStorageView: View {
+    @AppStorage("apptext") private var editorText: String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            TextEditor(text: $editorText)
+                .padding(30)
+        }
+        .modifier(StandardCustomFontText())
     }
 }
 
