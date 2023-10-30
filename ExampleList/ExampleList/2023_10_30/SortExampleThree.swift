2f17d63ba78d2e442f849e2d7fb729a5080ecf8b
NohJooYoung <nohjoo97@naver.com>
2023-10-30 17:57:13 +0900

    스텍 정렬 예제 첫번째 실습

--- ExampleList/ExampleList/2023_10_30/SortExampleThree.swift
+++ ExampleList/ExampleList/2023_10_30/SortExampleThree.swift
@@ -8,9 +8,44 @@
 import SwiftUI
 
 struct SortExampleThree: View {
+    @Environment(\.dismiss) private var dismiss
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                Button(action: {dismiss()}, label: {
+                    Text("< Back")
+                        .font(.custom("MaplestoryOTFLight", size: 25))
+                        .foregroundStyle(.black)
+                        .bold()
+                })
+                Spacer()
+            }
+            .padding(.horizontal, 20)
+                
+            Spacer()
+            VStack {
+                HStack(alignment: .threeAlignment) {
+                    Text("Hello")
+                        .modifier(StandardCustomFontTitle())
+                    Text("World")
+                        .modifier(StandardCustomFontText())
+                }
+                
+            }
+            Spacer()
+        }
+        .navigationBarBackButtonHidden()
     }
+}
+
+extension VerticalAlignment {
+    private enum ThreeAlignment: AlignmentID {
+        static func defaultValue(in d: ViewDimensions) -> CGFloat {
+            d[.bottom]
+        }
+    }
+    static let threeAlignment = VerticalAlignment(ThreeAlignment.self)
 }
 
 #Preview {
