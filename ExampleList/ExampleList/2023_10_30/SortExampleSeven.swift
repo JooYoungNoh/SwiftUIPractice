3186d7028392476e9ee6ad0c91fe67916f6942bd
NohJooYoung <nohjoo97@naver.com>
2023-10-30 22:36:33 +0900

    스택 정렬 다섯번째 실습

--- ExampleList/ExampleList/2023_10_30/SortExampleSeven.swift
+++ ExampleList/ExampleList/2023_10_30/SortExampleSeven.swift
@@ -8,8 +8,49 @@
 import SwiftUI
 
 struct SortExampleSeven: View {
+    @Environment(\.dismiss) private var dismiss
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                Button(action: {dismiss()}, label: {
+                    Text("< Back")
+                        .font(.custom("MaplestoryOTFLight", size: 25))
+                        .foregroundStyle(.black)
+                        .bold()
+                })
+                Spacer()
+            }
+            .padding(.horizontal, 20)
+                
+            Spacer()
+            HStack(alignment: .center) {
+                Image("pretty")
+                    .resizable()
+                    .frame(maxWidth: 100, maxHeight: 100)
+                    .cornerRadius(20)
+                    .aspectRatio(contentMode: .fit)
+                    .alignmentGuide(VerticalAlignment.center){
+                        $0[VerticalAlignment.bottom] + 30
+                    }
+                Image("pretty")
+                    .resizable()
+                    .frame(maxWidth: 100, maxHeight: 100)
+                    .cornerRadius(20)
+                    .aspectRatio(contentMode: .fit)
+                Image("pretty")
+                    .resizable()
+                    .frame(maxWidth: 100, maxHeight: 100)
+                    .cornerRadius(20)
+                    .aspectRatio(contentMode: .fit)
+                    .alignmentGuide(VerticalAlignment.center){
+                        $0[VerticalAlignment.top] - 30
+                    }
+                
+            }
+            Spacer()
+        }
+        .navigationBarBackButtonHidden()
     }
 }
 
