55a5f1a44bd80f154d4f4e5426c1141942f97ccf
NohJooYoung <nohjoo97@naver.com>
2023-10-30 15:02:22 +0900

    2023_10_30 화면 정렬 실습 first commit

---
+++ ExampleList/ExampleList/2023_10_30/SortExampleOne.swift
@@ -0,0 +1,18 @@
+//
+//  SortExampleOne.swift
+//  ExampleList
+//
+//  Created by 노주영 on 2023/10/30.
+//
+
+import SwiftUI
+
+struct SortExampleOne: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SortExampleOne()
+}
