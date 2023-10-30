82c852766e563e12a8feaebabe89e40802a3b819
NohJooYoung <nohjoo97@naver.com>
2023-10-30 22:30:23 +0900

    스택 정렬 네번째 실습

--- ExampleList/ExampleList/2023_10_30/SortExampleSix.swift
+++ ExampleList/ExampleList/2023_10_30/SortExampleSix.swift
@@ -8,8 +8,47 @@
 import SwiftUI
 
 struct SortExampleSix: View {
+    @Environment(\.dismiss) private var dismiss
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                Button(action: {dismiss()}, label: {
+                    Text("< Back")
+                        .font(.custom("MaplestoryOTFLight", size: 25))
+                        .foregroundStyle(.black)
+                        .bold()
+                })
+                Spacer()
+            }
+            .padding(.horizontal, 20)
+                
+            Spacer()
+            HStack(alignment: .center) {
+                VStack {
+                    Text("Hello")
+                        .modifier(StandardCustomFontTitle())
+                        .background(.red)
+                        
+                }
+                
+                VStack {
+                    Text("World")
+                        .modifier(StandardCustomFontText())
+                        .background(.blue)
+                        .alignmentGuide(HorizontalAlignment.center, computeValue: { d in
+                            d[HorizontalAlignment.leading] - 180
+                        })
+                }
+                .alignmentGuide(VerticalAlignment.center, computeValue: { d in
+                    d[VerticalAlignment.bottom] - 22
+                })
+            }
+            
+            .padding()
+            Spacer()
+        }
+        .navigationBarBackButtonHidden()
     }
 }
 
