ecdd13d27d01a600fc0666457ac7bce7ffe15c94
NohJooYoung <nohjoo97@naver.com>
2023-10-30 16:32:26 +0900

    화면 정렬 두번째 실습

--- ExampleList/ExampleList/2023_10_30/SortExampleTwo.swift
+++ ExampleList/ExampleList/2023_10_30/SortExampleTwo.swift
@@ -8,8 +8,72 @@
 import SwiftUI
 
 struct SortExampleTwo: View {
+    @Environment(\.dismiss) private var dismiss
+    
+    @State private var change: Bool = true
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                Button(action: {dismiss()}, label: {
+                    Text("< Back")
+                        .font(.custom("MaplestoryOTFLight", size: 25))
+                        .foregroundStyle(.black)
+                        .bold()
+                })
+                Spacer()
+                
+            }
+            .padding(.horizontal, 20)
+            
+            Spacer()
+            VStack(spacing: 50) {
+                Image(change ? "pretty" : "cute")
+                    .resizable()
+                    .frame(maxWidth: 200, maxHeight: 200)
+                    .cornerRadius(20)
+                    .aspectRatio(contentMode: .fit)
+                
+                HStack(spacing: 150) {
+                    Button(action: prettyUp, label: {
+                        Text("Up")
+                            .modifier(StandardCustomFontText())
+                            .bold()
+                            .padding()
+                    })
+                    .frame(maxWidth: 100, maxHeight: 40)
+                    .border(.gray, width: 4)
+                    .cornerRadius(5)
+                    
+                    
+                    Button(action: prettyDown, label: {
+                        Text("Down")
+                            .modifier(StandardCustomFontText())
+                            .bold()
+                            .padding()
+                    })
+                    .frame(maxWidth: 100, maxHeight: 40)
+                    .border(.gray, width: 4)
+                    .cornerRadius(5)
+                }
+                
+                Image(change ? "cute" : "pretty")
+                    .resizable()
+                    .frame(maxWidth: 200, maxHeight: 200)
+                    .cornerRadius(20)
+                    .aspectRatio(contentMode: .fit)
+            }
+            .padding()
+            Spacer()
+        }
+        .navigationBarBackButtonHidden()
+    }
+    func prettyUp() {
+        change = true
+    }
+    
+    func prettyDown() {
+        change = false
     }
 }
 
