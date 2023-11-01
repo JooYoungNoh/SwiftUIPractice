4ef8d40fd8e50c6293c56e641f1e2bce6137914b
NohJooYoung <nohjoo97@naver.com>
2023-11-01 15:31:39 +0900

    2023_11_01 네비게이션 실습 first commit

---
+++ ExampleList/ExampleList/2023_11_01/NaviFirst.swift
@@ -0,0 +1,18 @@
+//
+//  NaviFirst.swift
+//  ExampleList
+//
+//  Created by 노주영 on 2023/11/01.
+//
+
+import SwiftUI
+
+struct NaviFirst: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NaviFirst()
+}
