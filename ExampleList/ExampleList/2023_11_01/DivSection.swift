7c5adb829c1ff0eb9f3466812a6075e22b5bc01b
NohJooYoung <nohjoo97@naver.com>
2023-11-01 13:04:31 +0900

    2023_11_01 섹션 나누기 실습 first commit

---
+++ ExampleList/ExampleList/2023_11_01/DivSection.swift
@@ -0,0 +1,18 @@
+//
+//  DivSection.swift
+//  ExampleList
+//
+//  Created by 노주영 on 2023/11/01.
+//
+
+import SwiftUI
+
+struct DivSection: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DivSection()
+}
