0d71621eb3ca77857c08abceea645136b190430c
NohJooYoung <nohjoo97@naver.com>
2023-11-01 10:11:50 +0900

    2023_11_01 리스트 만들기 실습 first commit

---
+++ ExampleList/ExampleList/2023_11_01/ListPractice.swift
@@ -0,0 +1,18 @@
+//
+//  ListPractice.swift
+//  ExampleList
+//
+//  Created by 노주영 on 2023/11/01.
+//
+
+import SwiftUI
+
+struct ListPractice: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListPractice()
+}
