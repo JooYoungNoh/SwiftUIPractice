db8e6e947d3d6aab0bccb77d568bbaddb68ff67c
NohJooYoung <nohjoo97@naver.com>
2023-11-02 11:07:18 +0900

    2023_11_02 실습 first commit

---
+++ ExampleList/ExampleList/2023_11_02/ListNaviDetail.swift
@@ -0,0 +1,18 @@
+//
+//  ListNaviDetail.swift
+//  ExampleList
+//
+//  Created by 노주영 on 2023/11/02.
+//
+
+import SwiftUI
+
+struct ListNaviDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListNaviDetail()
+}
