b32342e48c12a8982fa7278f5ee82ac69df5bb9f
NohJooYoung <nohjoo97@naver.com>
2023-11-02 11:14:34 +0900

    차 세부사항 화면 구성

--- ExampleList/ExampleList/2023_11_02/ListNaviDetail.swift
+++ ExampleList/ExampleList/2023_11_02/ListNaviDetail.swift
@@ -8,11 +8,50 @@
 import SwiftUI
 
 struct ListNaviDetail: View {
+    @Environment(\.dismiss) private var dismiss
+    
+    let selectedCar: Car
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                Button(action: {dismiss()}, label: {
+                    Text("< Back")
+                        .font(.custom("NotoSansKR-Regular", size: 25))
+                        .foregroundStyle(.black)
+                        .bold()
+                })
+                Spacer()
+                
+            }
+            .padding(.horizontal, 20)
+            
+            Form {
+                Section {
+                    Image(selectedCar.imageName)
+                        .resizable()
+                        .cornerRadius(12.0)
+                        .aspectRatio(contentMode: .fit)
+                        .padding()
+                    
+                    Text(selectedCar.name)
+                        .font(.custom("NotoSansKR-Regular", size: 25))
+                    
+                    Text(selectedCar.description)
+                        .font(.body)
+                    
+                    HStack {
+                        Text("Hybrid").font(.custom("NotoSansKR-Bold", size: 30))
+                        Spacer()
+                        Image(systemName: selectedCar.isHybrid ?  "checkmark.circle" : "xmark.circle")
+                    }
+                } header: {
+                    Text("Car Details")
+                }
+                
+            }
+        }
+        .navigationBarBackButtonHidden()
     }
 }
 
-#Preview {
-    ListNaviDetail()
-}
