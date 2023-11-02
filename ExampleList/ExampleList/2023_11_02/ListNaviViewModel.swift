9110c89c48280a5cccea92eeeca1645f9dc9456e
NohJooYoung <nohjoo97@naver.com>
2023-11-02 10:14:13 +0900

    2023_11_02 실습 뷰모델 파일 생성

---
+++ ExampleList/ExampleList/2023_11_02/ListNaviViewModel.swift
@@ -0,0 +1,39 @@
+//
+//  ListNaviViewModel.swift
+//  ExampleList
+//
+//  Created by 노주영 on 2023/11/02.
+//
+
+import Foundation
+
+class ListNaviViewModel: ObservableObject {
+    @Published var cars: [Car] = []
+    
+    init(cars: [Car]) {
+        self.cars = cars
+    }
+    
+    
+    //JSON 파일 로드하기
+    func loadJson<T: Decodable>(_ filename: String) -> T {
+        var data: Data
+        
+        guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
+        else {
+            fatalError("파일을 찾을 수 없습니다")
+        }
+        
+        do {
+            data = try Data(contentsOf: file)
+        } catch {
+            fatalError("데이터 변환에 실패했습니다")
+        }
+        
+        do {
+            return try JSONDecoder().decode(T.self, from: data)
+        } catch {
+            fatalError("파싱에 실패했습니다")
+        }
+    }
+}
