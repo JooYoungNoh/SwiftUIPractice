0aa6aac46752e4691410c5bbee63214343cb137d
NohJooYoung <nohjoo97@naver.com>
2023-11-02 11:02:20 +0900

    차 목록 화면 구성

--- ExampleList/ExampleList/2023_11_02/ListNavi.swift
+++ ExampleList/ExampleList/2023_11_02/ListNavi.swift
@@ -8,8 +8,57 @@
 import SwiftUI
 
 struct ListNavi: View {
+    @Environment(\.dismiss) private var dismiss
+    @StateObject var carVM: ListNaviViewModel = ListNaviViewModel()
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                Button(action: {dismiss()}, label: {
+                    Text("< Back")
+                        .font(.custom("NotoSansKR-Regular", size: 25))
+                        .foregroundStyle(.black)
+                        .bold()
+                })
+                Spacer()
+                
+            }
+            .padding(.horizontal, 20)
+            
+            List {
+                Section(content: {
+                    ForEach(0..<carVM.cars.count, id: \.self) { i in
+                        ListCell(car: carVM.cars[i])
+                    }
+                },header: {
+                    Text("Car Type")
+                        .font(.custom("NotoSansKR-Bold", size: 25))
+                        .foregroundStyle(.black)
+                        .bold()
+                })
+            }
+        }
+        .navigationBarBackButtonHidden()
+        .onAppear {
+            Task {
+                carVM.cars = await carVM.loadJson("carData.json")
+            }
+        }
+    }
+}
+
+struct ListCell: View {
+    var car: Car
+    
+    var body: some View {
+        HStack {
+            Image(car.imageName)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 100, height: 60)
+            Text(car.name)
+                .font(.custom("NotoSansKR-Regular", size: 20))
+        }
     }
 }
 
