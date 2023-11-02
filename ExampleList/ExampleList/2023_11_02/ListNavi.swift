0ee81c5b9e889883ceda791392549527fc447fe0
NohJooYoung <nohjoo97@naver.com>
2023-11-02 09:24:14 +0900

    2023_11_02 리스트 네비게이션 실습 first commit

---
+++ ExampleList/ExampleList/2023_11_02/ListNavi.swift
@@ -0,0 +1,18 @@
+//
+//  ListNavi.swift
+//  ExampleList
+//
+//  Created by 노주영 on 2023/11/02.
+//
+
+import SwiftUI
+
+struct ListNavi: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListNavi()
+}
