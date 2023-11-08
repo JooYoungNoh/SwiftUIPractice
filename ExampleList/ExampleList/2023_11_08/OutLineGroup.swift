ba1b3526ae994d02b83571f967132ff1c9350b6c
NohJooYoung <nohjoo97@naver.com>
2023-11-08 14:11:45 +0900

    모델 추가

--- ExampleList/ExampleList/2023_11_08/OutLineGroup.swift
+++ ExampleList/ExampleList/2023_11_08/OutLineGroup.swift
@@ -7,6 +7,13 @@
 
 import SwiftUI
 
+struct CarInfo: Identifiable {
+    let id = UUID()
+    var name: String
+    var image: String
+    var children: [CarInfo]?
+}
+
 struct OutLineGroup: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
