676b18221e5622186368ae42dfa23155b5730987
NohJooYoung <nohjoo97@naver.com>
2023-11-08 17:39:06 +0900

    OutlineGroup 실습 예제 3 화면 구성

--- ExampleList/ExampleList/2023_11_08/GroupPractice.swift
+++ ExampleList/ExampleList/2023_11_08/GroupPractice.swift
@@ -21,6 +21,12 @@ struct MenuItem: Identifiable {
     var children: [MenuItem]?
 }
 
+struct ImageItem: Identifiable {
+    let id = UUID()
+    var name: String
+    var imageLink: String
+}
+
 let sampleOne = [
     GroupOneModel(title: "A", children: [
         GroupOneModel(title: "B"),
@@ -42,8 +48,20 @@ let sampleTwo = [
     MenuItem(title: "Salad", price: 3000)
 ]
 
+
 struct GroupPractice: View {
     @Environment(\.dismiss) private var dismiss
+    
+    @State private var normalState: Bool = true
+    @State private var reverseState: Bool = false
+    @State private var randomState: Bool = false
+    
+    @State private var imageList: [ImageItem] = [
+        ImageItem(name: "조유리", imageLink: "https://thumbnews.nateimg.co.kr/view610///news.nateimg.co.kr/orgImg/nn/2021/10/12/202110121604440010_1.jpg"),
+        ImageItem(name: "카리나", imageLink: "https://image.fmkorea.com/files/attach/new2/20211226/3655109/3035547727/4197422423/25d648fe733c6d47eb95160f31ed02ff.jpeg"),
+        ImageItem(name: "아이유", imageLink: "https://cdn2.ppomppu.co.kr/zboard/data3/2014/0930/m_1412087358_3415783392_f0263985.jpg"),
+        ImageItem(name: "윈터", imageLink: "https://upload3.inven.co.kr/upload/2022/03/15/bbs/i16485681820.jpg?MW=800")
+    ]
     
     var body: some View {
         VStack {
@@ -60,32 +78,120 @@ struct GroupPractice: View {
             .padding(.horizontal, 20)
             
             List {
-                ForEach(sampleOne) { sample in
-                    OutlineGroup(sample, children: \.children) { children in
-                        Text(children.title)
+                Section {
+                    ForEach(sampleOne) { sample in
+                        OutlineGroup(sample, children: \.children) { children in
+                            Text(children.title)
+                                .modifier(StandardCustomSimpleText())
+                        }
+                    }
+                } header: {
+                    Text("실습 1")
+                        .modifier(StandardCustomSimpleText())
+                }
+                
+                Section {
+                    ForEach(sampleTwo) { sample in
+                        OutlineGroup(sample, children: \.children) { children in
+                            HStack {
+                                Text(children.title)
+                                    .frame(width: 100, height: 20, alignment: .leading)
+                                    .modifier(StandardCustomSimpleText())
+                                    .padding(.trailing, 10)
+                                Text("\(children.price)")
+                                    .modifier(StandardCustomSimpleText())
+                            }
+                        }
+                    }
+                } header: {
+                    Text("실습 2")
+                        .modifier(StandardCustomSimpleText())
+                }
+                
+                Section {
+                    DisclosureGroup {
+                        HStack {
+                            Text("Normal")
+                                .modifier(StandardCustomSimpleText())
+                            Spacer()
+                            Button(action: {
+                                imageList = [ImageItem(name: "조유리", imageLink: "https://thumbnews.nateimg.co.kr/view610///news.nateimg.co.kr/orgImg/nn/2021/10/12/202110121604440010_1.jpg"),
+                                ImageItem(name: "카리나", imageLink: "https://image.fmkorea.com/files/attach/new2/20211226/3655109/3035547727/4197422423/25d648fe733c6d47eb95160f31ed02ff.jpeg"),
+                                ImageItem(name: "아이유", imageLink: "https://cdn2.ppomppu.co.kr/zboard/data3/2014/0930/m_1412087358_3415783392_f0263985.jpg"),
+                                ImageItem(name: "윈터", imageLink: "https://upload3.inven.co.kr/upload/2022/03/15/bbs/i16485681820.jpg?MW=800")
+                                ]
+                            }, label: {
+                                Text("Click")
+                                    .padding(5)
+                                    .bold()
+                            })
+                            .background(Color(uiColor: UIColor.systemGray6))
+                            .cornerRadius(5)
+                        }
+                        
+                        HStack {
+                            Text("Reverse")
+                                .modifier(StandardCustomSimpleText())
+                            Spacer()
+                            Button(action: { 
+                                imageList.reverse()
+                            }, label: {
+                                Text("Click")
+                                    .padding(5)
+                                    .bold()
+                            })
+                            .background(Color(uiColor: UIColor.systemGray6))
+                            .cornerRadius(5)
+                        }
+                        
+                        HStack {
+                            Text("Random")
+                                .modifier(StandardCustomSimpleText())
+                            Spacer()
+                            Button(action: {
+                                imageList.shuffle()
+                            }, label: {
+                                Text("Click")
+                                    .padding(5)
+                                    .bold()
+                            })
+                            .background(Color(uiColor: UIColor.systemGray6))
+                            .cornerRadius(5)
+                        }
+                            
+                        
+                    } label: {
+                        Label("Image Sort", systemImage: "photo")
                             .modifier(StandardCustomSimpleText())
                     }
+                } header: {
+                    Text("실습 3")
+                        .modifier(StandardCustomSimpleText())
                 }
-            }
-            .listStyle(InsetListStyle())
-            .padding(.bottom, 20)
-            
-            List {
-                ForEach(sampleTwo) { sample in
-                    OutlineGroup(sample, children: \.children) { children in
-                        HStack {
-                            Text(children.title)
-                                .frame(width: 100, height: 40, alignment: .leading)
-                                .modifier(StandardCustomSimpleText())
-                                .padding(.trailing, 10)
-                            Text("\(children.price)")
-                                .modifier(StandardCustomSimpleText())
+                
+                ScrollView(.horizontal) {
+                    HStack {
+                        ForEach(imageList) { i in
+                            VStack {
+                                AsyncImage(url: URL(string: i.imageLink)!) {
+                                    image in
+                                    image
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .cornerRadius(12.0)
+                                        .frame(width: 100, height: 100)
+                                } placeholder: {
+                                    ProgressView()
+                                        .frame(width: 100, height: 100)
+                                }
+                                
+                                Text(i.name)
+                                    .modifier(StandardCustomSimpleText())
+                            }
                         }
                     }
                 }
             }
-            .listStyle(InsetListStyle())
-            .padding(.bottom, 20)
         }
         .navigationBarBackButtonHidden()
     }
