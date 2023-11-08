2c192e664342fb889655d4ea97b97a0c9c376677
NohJooYoung <nohjoo97@naver.com>
2023-11-08 15:38:45 +0900

    OutlineGroup 실습 예제 1 화면 구성

--- ExampleList/ExampleList/2023_11_08/GroupPractice.swift
+++ ExampleList/ExampleList/2023_11_08/GroupPractice.swift
@@ -7,8 +7,50 @@
 
 import SwiftUI
 
+struct GroupOneModel: Identifiable{
+    let id = UUID()
+    var title: String
+    var children: [GroupOneModel]?
+    
+}
+
+let sampleOne = [
+    GroupOneModel(title: "A", children: [
+        GroupOneModel(title: "B"),
+        GroupOneModel(title: "C", children: [
+            GroupOneModel(title: "D"),
+            GroupOneModel(title: "E")
+        ])
+    ]),
+    GroupOneModel(title: "F")
+]
+
 struct GroupPractice: View {
+    @Environment(\.dismiss) private var dismiss
+    
     var body: some View {
+        VStack {
+            HStack {
+                Button(action: {dismiss()}, label: {
+                    Text("< Back")
+                        .font(.custom("NotoSansKR-Bold", size: 25))
+                        .foregroundStyle(.black)
+                        .bold()
+                    
+                })
+                Spacer()
+            }
+            .padding(.horizontal, 20)
+            List {
+                ForEach(sampleOne) { sample in
+                    OutlineGroup(sample, children: \.children) { children in
+                        Text(children.title)
+                            .modifier(StandardCustomSimpleText())
+                    }
+                }
+            }
+            .listStyle(InsetListStyle())
+        }
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
