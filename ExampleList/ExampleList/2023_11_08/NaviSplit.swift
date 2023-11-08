6be16e650662d009a86b40afd32bae71b2ce377e
NohJooYoung <nohjoo97@naver.com>
2023-11-08 10:50:24 +0900

    분할 네비게이션 뷰 실습 화면 구성(아이패드 및 아이폰 15 pro max 가로모드 가능)

--- ExampleList/ExampleList/2023_11_08/NaviSplit.swift
+++ ExampleList/ExampleList/2023_11_08/NaviSplit.swift
@@ -7,9 +7,85 @@
 
 import SwiftUI
 
+struct IconCategory: Identifiable, Hashable {
+    let id = UUID()
+    var categoryName: String
+    var images: [String]
+}
+
 struct NaviSplit: View {
+    @Environment(\.dismiss) private var dismiss
+    
+    @State private var categories = [
+        IconCategory(categoryName: "Folders", images: ["questionmark.folder.ar",
+                                                       "questionmark.folder",
+                                                       "questionmark.folder.fill.ar",
+                                                       "folder.fill.badge.gear",
+                                                       "questionmark.folder.fill"]),
+        IconCategory(categoryName: "Circles", images:
+                        ["book.circle",
+                         "books.vertical.circle",
+                         "books.vertical.circle.fill",
+                         "book.circle.fill",
+                         "book.closed.circle"]),
+        IconCategory(categoryName: "Clouds", images:
+                        ["cloud.rain",
+                         "cloud",
+                         "cloud.drizzle.fill",
+                         "cloud.fill",
+                         "cloud.drizzle"])
+        
+    ]
+    @State private var selectedCategory: IconCategory?
+    @State private var selectedImage: String?
+    @State private var columnVisibility = NavigationSplitViewVisibility.detailOnly
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            NavigationSplitView(columnVisibility: $columnVisibility) {
+                List(categories, selection: $selectedCategory) { category in
+                    Text(category.categoryName).tag(category)
+                }
+            } content: {
+                if let selectedCategory {
+                    List(selectedCategory.images, id: \.self, selection: $selectedImage) { image in
+                        HStack {
+                            Image(systemName: image)
+                            Text(image)
+                        }
+                        .tag(image)
+                    }
+                } else {
+                    Text("Select a category")
+                }
+            } detail: {
+                if let selectedImage {
+                    Image(systemName: selectedImage)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .padding()
+                } else {
+                    Text("Select a image")
+                }
+            }
+            .navigationSplitViewStyle(.balanced)
+            
+            if let selectedCategory, let selectedImage {
+                HStack {
+                    Text(selectedCategory.categoryName)
+                        .padding()
+                    Text(selectedImage)
+                        .padding()
+                }
+            } else {
+                HStack {
+                    Text("Select a category")
+                        .padding()
+                    Text("Select a image")
+                        .padding()
+                }
+            }
+        }
     }
 }
 
