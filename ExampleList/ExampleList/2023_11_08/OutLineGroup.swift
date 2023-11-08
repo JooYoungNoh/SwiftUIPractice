ac05af7d009107813ae4ed6180c700cd50fc06a2
NohJooYoung <nohjoo97@naver.com>
2023-11-08 14:19:10 +0900

    샘플 데이터 추가

--- ExampleList/ExampleList/2023_11_08/OutLineGroup.swift
+++ ExampleList/ExampleList/2023_11_08/OutLineGroup.swift
@@ -14,6 +14,35 @@ struct CarInfo: Identifiable {
     var children: [CarInfo]?
 }
 
+let carItems: [CarInfo] = [
+
+    CarInfo(name: "Hybrid Cars", image: "leaf.fill", children: [
+        CarInfo(name: "Toyota", image: "car.circle", children : [
+            CarInfo(name: "Prius", image: "leaf.fill"),
+            CarInfo(name: "Highlander Hybrid", image: "leaf.fill"),
+            CarInfo(name: "Lexus", image: "car.circle", children: [
+                CarInfo(name: "Lexus RX", image: "leaf.fill"),
+                CarInfo(name: "Lexus NX", image: "leaf.fill")])
+        ]),
+        CarInfo(name: "Ford", image: "car.circle", children : [
+            CarInfo(name: "Fusion Energi", image: "leaf.fill"),
+            CarInfo(name: "Escape", image: "leaf.fill"),
+            CarInfo(name: "Volvo", image: "car.circle", children: [
+                CarInfo(name: "S90 Hybrid", image: "leaf.fill"),
+                CarInfo(name: "XC90 Hybrid", image: "leaf.fill")])
+        ]),
+    ]),
+
+    CarInfo(name: "Electric Cars", image: "bolt.car.fill", children: [
+        CarInfo(name: "Tesla", image: "car.circle", children : [
+            CarInfo(name: "Model 3", image: "bolt.car.fill")
+        ]),
+        CarInfo(name: "Karma", image: "car.circle", children : [
+            CarInfo(name: "Revero GT", image: "bolt.car.fill")
+        ])
+    ])
+]
+
 struct OutLineGroup: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
