2186149ba4cafa000864907560aa0536bed28dc4
NohJooYoung <nohjoo97@naver.com>
2023-11-08 18:19:36 +0900

    그리드 실습 예제 화면 구성

--- ExampleList/ExampleList/2023_11_08/GridPratice.swift
+++ ExampleList/ExampleList/2023_11_08/GridPratice.swift
@@ -8,8 +8,44 @@
 import SwiftUI
 
 struct GridPratice: View {
+    @Environment(\.dismiss) private var dismiss
+    
+    @State private var layouts: [[GridItem]] = [[GridItem()], [GridItem(), GridItem()], [GridItem(),GridItem(),GridItem()], [GridItem(),GridItem(),GridItem(),GridItem()], [GridItem(),GridItem(),GridItem(),GridItem(),GridItem()]]
+    @State private var rotation: Double = 0.0
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack {
+            HStack {
+                Button(action: {dismiss()}, label: {
+                    Text("< Back")
+                        .font(.custom("NotoSansKR-Bold", size: 25))
+                        .foregroundStyle(.black)
+                        .bold()
+                    
+                })
+                Spacer()
+            }
+            .padding(.horizontal, 20)
+            
+            Slider(value: $rotation, in: 0...4, step: 1)
+                .tint(.black)
+                .padding()
+            
+            ScrollView(.vertical) {
+                LazyVGrid(columns: layouts[Int(rotation)], spacing: 5) {
+                    ForEach((0...4), id: \.self) { index in
+                        Rectangle()
+                            .frame(width: 60, height: 40)
+                            .cornerRadius(8)
+                            .tint(.red)
+                        
+                    }
+                }
+                .padding()
+            }
+        }
+        .navigationBarBackButtonHidden()
     }
 }
 
