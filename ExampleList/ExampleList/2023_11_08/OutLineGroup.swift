8ba2b034e93945e2bfbc8bac4aa21a0bc76eaa9b
NohJooYoung <nohjoo97@naver.com>
2023-11-08 14:07:30 +0900

    2023_11_08 실습 예제 4 first commit

---
+++ ExampleList/ExampleList/2023_11_08/OutLineGroup.swift
@@ -0,0 +1,18 @@
+//
+//  OutLineGroup.swift
+//  ExampleList
+//
+//  Created by 노주영 on 2023/11/08.
+//
+
+import SwiftUI
+
+struct OutLineGroup: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OutLineGroup()
+}
