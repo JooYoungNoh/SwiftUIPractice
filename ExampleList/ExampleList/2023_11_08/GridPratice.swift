942ffb78148ccff267ef674a1254b8d6851468db
NohJooYoung <nohjoo97@naver.com>
2023-11-08 17:41:47 +0900

    2023_11_08 그리드 실습 예제 first commit

---
+++ ExampleList/ExampleList/2023_11_08/GridPratice.swift
@@ -0,0 +1,18 @@
+//
+//  GridPratice.swift
+//  ExampleList
+//
+//  Created by 노주영 on 2023/11/08.
+//
+
+import SwiftUI
+
+struct GridPratice: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GridPratice()
+}
