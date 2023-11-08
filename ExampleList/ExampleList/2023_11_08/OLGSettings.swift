aae2f4945d8d15597da8f7587ed83dff40abd166
NohJooYoung <nohjoo97@naver.com>
2023-11-08 14:49:18 +0900

    2023_11_08 실습 first commit

---
+++ ExampleList/ExampleList/2023_11_08/OLGSettings.swift
@@ -0,0 +1,18 @@
+//
+//  OLGSettings.swift
+//  ExampleList
+//
+//  Created by 노주영 on 2023/11/08.
+//
+
+import SwiftUI
+
+struct OLGSettings: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OLGSettings()
+}
