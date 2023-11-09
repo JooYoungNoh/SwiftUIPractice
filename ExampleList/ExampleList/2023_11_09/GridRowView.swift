7e6cfae960f26c304e2a5f525406a12a189bd365
NohJooYoung <nohjoo97@naver.com>
2023-11-09 11:17:59 +0900

    그리드 로우 실습 화면 구성

--- ExampleList/ExampleList/2023_11_09/GridRowView.swift
+++ ExampleList/ExampleList/2023_11_09/GridRowView.swift
@@ -8,8 +8,90 @@
 import SwiftUI
 
 struct GridRowView: View {
+    @Environment(\.dismiss) private var dismiss
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                Button(action: {dismiss()}, label: {
+                    Text("< Back")
+                        .font(.custom("NotoSansKR-Bold", size: 25))
+                        .foregroundStyle(.black)
+                        .bold()
+                    
+                })
+                Spacer()
+            }
+            .padding(.horizontal, 20)
+            
+            Spacer()
+            Grid(horizontalSpacing: 15) {
+                GridRow(alignment: .top) {
+                    Text("\(0)")
+                        .frame(minWidth: 50, maxWidth: .infinity, minHeight: 100)
+                        .background(Color(uiColor: UIColor.systemGray2))
+                        .cornerRadius(8)
+                    Image(systemName: "record.circle.fill")
+                        .gridColumnAlignment(.leading)
+                    Image(systemName: "record.circle.fill")
+                    Image(systemName: "record.circle.fill")
+                    Text("\(0)")
+                        .frame(minWidth: 50, maxWidth: .infinity, minHeight: 100)
+                        .background(Color(uiColor: UIColor.systemGray2))
+                        .cornerRadius(8)
+                }
+                .font(.largeTitle)
+                
+                GridRow(alignment: .bottom) {
+                    Text("\(0)")
+                        .frame(minWidth: 50, maxWidth: .infinity, minHeight: 100)
+                        .background(Color(uiColor: UIColor.systemGray2))
+                        .cornerRadius(8)
+                    Image(systemName: "record.circle.fill")
+                    Image(systemName: "record.circle.fill")
+                    Image(systemName: "record.circle.fill")
+                    Text("\(0)")
+                        .frame(minWidth: 50, maxWidth: .infinity, minHeight: 100)
+                        .background(Color(uiColor: UIColor.systemGray2))
+                        .cornerRadius(8)
+                }
+                .font(.largeTitle)
+                
+                GridRow {
+                    ForEach(1...5, id: \.self) { index in
+                        if index%2 == 1 {
+                            Text("\(index)")
+                                .frame(minWidth: 50, maxWidth: .infinity, minHeight: 100)
+                                .background(Color(uiColor: UIColor.systemGray2))
+                                .cornerRadius(8)
+                        } else {
+                            //빈 셀 추가
+                            Color.clear.gridCellUnsizedAxes([.horizontal, .vertical])
+                        }
+                    }
+                }
+                
+                Text("\(16)")
+                    .frame(minWidth: 50, maxWidth: .infinity, minHeight: 100)
+                    .background(Color(uiColor: UIColor.systemGray2))
+                    .cornerRadius(8)
+                
+                GridRow {
+                    Text("\(17)")
+                        .frame(minWidth: 50, maxWidth: .infinity, minHeight: 100)
+                        .background(Color(uiColor: UIColor.systemGray2))
+                        .cornerRadius(8)
+                        .gridCellColumns(2)
+                    Text("\(18)")
+                        .frame(minWidth: 50, maxWidth: .infinity, minHeight: 100)
+                        .background(Color(uiColor: UIColor.systemGray2))
+                        .cornerRadius(8)
+                        .gridCellColumns(5)
+                }
+            }
+            Spacer()
+        }
+        .navigationBarBackButtonHidden()
     }
 }
 
