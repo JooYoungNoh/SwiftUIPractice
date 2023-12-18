f73f59b6afde9f326f8bbe0b58f4f2f4a61a8995
NohJooYoung <nohjoo97@naver.com>
2023-12-18 10:19:33 +0900

    design: 기본 UI 생성

--- UITestPractice/UITestPractice/ContentView.swift
+++ UITestPractice/UITestPractice/ContentView.swift
@@ -8,12 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var welcomeText: String = "로그인 해주세요"
+    @State var loginText: String = "로그인"
+    @State var isShow: Bool = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 30) {
+            Text(welcomeText)
+                .font(.title)
+                .bold()
+            
+            Button {
+                isShow.toggle()
+            } label: {
+                Text(loginText)
+                    .padding()
+                    .font(.system(size: 25, weight: .semibold))
+                    .foregroundStyle(loginText == "로그인" ? .black : .red)
+                    .background(Color(uiColor: .systemGray5))
+                    .cornerRadius(10)
+            }
+
         }
         .padding()
     }
