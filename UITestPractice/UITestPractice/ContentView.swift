09c6754a89a6496eefc272b46af10770c079d29d
NohJooYoung <nohjoo97@naver.com>
2023-12-18 09:48:57 +0900

    2023_12_18 UI Test 실습 initial commit

---
+++ UITestPractice/UITestPractice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UITestPractice
+//
+//  Created by 노주영 on 12/18/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
---
+++ UITestPractice/UITestPractice/UITestPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  UITestPracticeApp.swift
+//  UITestPractice
+//
+//  Created by 노주영 on 12/18/23.
+//
+
+import SwiftUI
+
+@main
+struct UITestPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
