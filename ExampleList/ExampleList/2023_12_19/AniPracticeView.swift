c076202c8b5f6d5f06852aa1022c78a3723e2ce0
NohJooYoung <nohjoo97@naver.com>
2023-12-19 10:07:26 +0900

    feat: 기본 도형 그리기

--- ExampleList/ExampleList/2023_12_19/AniPracticeView.swift
+++ ExampleList/ExampleList/2023_12_19/AniPracticeView.swift
@@ -9,10 +9,47 @@ import SwiftUI
 
 struct AniPracticeView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ShapeView()
     }
 }
 
-#Preview {
-    AniPracticeView()
+struct ShapeView: View {
+    var body: some View {
+        ScrollView {
+            VStack(spacing: 50) {
+                Circle()
+                    .fill(.red)
+                    .frame(width: 200, height: 200)
+                
+                Capsule()
+                    .stroke(lineWidth: 10)
+                    .foregroundStyle(.blue)
+                    .frame(width: 200, height: 100)
+                
+                RoundedRectangle(cornerRadius: 20)
+                    .stroke(style: StrokeStyle(lineWidth: 8, dash: [CGFloat(10)]))
+                    .frame(width: 200, height: 100)
+                
+                Ellipse()
+                    .stroke(style: StrokeStyle(lineWidth: 8, dash: [CGFloat(10), CGFloat(5), CGFloat(2)], dashPhase: 10))
+                    .frame(width: 200, height: 100)
+                
+                Text("테두리 오버레이 사용")
+                Ellipse()
+                    .fill(.red)
+                    .overlay {
+                        Ellipse()
+                            .stroke(.blue, lineWidth: 10)
+                    }
+                    .frame(width: 250, height: 150)
+                
+                Ellipse()
+                    .fill(.red)
+                    .border(.blue, width: 10)
+                    .frame(width: 250, height: 150)
+            }
+            .frame(width: UIScreen.main.bounds.width)
+            .padding()
+        }
+    }
 }
