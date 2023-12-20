b483737239a3c0c271dcbaacdad7191f0864010b
NohJooYoung <nohjoo97@naver.com>
2023-12-20 10:21:10 +0900

    feat: 클릭 시 애니메이션

--- ExampleList/ExampleList/2023_12_20/AnimationDemoTwoView.swift
+++ ExampleList/ExampleList/2023_12_20/AnimationDemoTwoView.swift
@@ -8,11 +8,46 @@
 import SwiftUI
 
 struct AnimationDemoTwoView: View {
+    @Namespace private var menuItemTransition
+    
+    @State var selectIndex: Int = 0
+    
+    let menuItems = ["Travel", "Nature", "Architecture"]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Spacer()
+            ForEach(menuItems.indices, id: \.self) { index in
+                if selectIndex == index {
+                    MenuView(title: menuItems[index])
+                        .background(Capsule().foregroundStyle(.purple))
+                        .matchedGeometryEffect(id: "menuItem", in: menuItemTransition)
+                        .onTapGesture {
+                            selectIndex = index
+                        }
+                } else {
+                    MenuView(title: menuItems[index])
+                        .background(Capsule().foregroundStyle(Color(uiColor: .systemGray4)))
+                        .onTapGesture {
+                            selectIndex = index
+                        }
+                }
+                Spacer()
+            }
+        }
+        .padding()
+        .animation(.easeInOut, value: selectIndex)
     }
 }
 
-#Preview {
-    AnimationDemoTwoView()
+struct MenuView: View {
+    var title: String
+    
+    var body: some View {
+        Text(title)
+            .padding(.horizontal)
+            .padding(.vertical, 5)
+            .foregroundStyle(.white)
+    }
 }
+
